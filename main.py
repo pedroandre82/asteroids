21eb4d681004498e21b8dda734a1f323d7cc593f
Pedro Silveira <pedro.com.br@gmail.com>
2026-02-14 01:21:18 -0300

    add rotation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -11,6 +12,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # create a infinite loop to keep the game running
     while True:
         log_state()
@@ -20,8 +26,14 @@ def main():
                 # pygame.quit()
                 return
 
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0
+        # print(f"Delta time: {dt:.4f} seconds")
     
 
 if __name__ == "__main__":
